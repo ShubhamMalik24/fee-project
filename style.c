8afeef422f471ea1f9edcea951d471204ea9db8f
ronak1114 <ronak1114.be23@chitkara.edu.in>
2024-05-15 01:16:26 +0530

    20

--- style.c
+++ style.c
@@ -14,3 +14,26 @@ color-scheme:orange ;
 text-align: center;
 color:indigo;
 background-image:linear-gradient(darkkhaki,lemonchiffon,papayawhip,peachpuff,darkkhaki);}
+
+a:link{color:rgb(236, 157, 77);}
+a:visited{color:firebrick;}
+a:hover{color:rgb(143, 127, 104);}
+a:active{color:yellowgreen;}
+
+.container{display: flex;border: 5px solid yellowgreen;width:5000px; height:1330px}
+.fitem{ border: 50px; width: 1000px; color:olivedrab;padding: 15px; 
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+font-size:larger;
+font-weight: 200;}
+hr{border-color: yellowgreen; }
+
+
+
+.flip-box {
+    background-color: transparent;
+    width: 700px;
+    height: 400px;
+    border: 5px solid #f1f1f1;
+    perspective: 1000px; 
+  }
+
