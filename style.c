930db6cb0dbfe1acd454a7fb436c8b77324edc1d
shubham malik <malik20shubham@gmail.com>
2024-05-15 01:15:01 +0530

    19

---
+++ style.c
@@ -0,0 +1,16 @@
+.glow{
+    box-shadow: 10px 10px 10px firebrick;
+}
+
+.glow:hover{
+    box-shadow: 5px 5px 15px firebrick;
+    animation: ease-in 1s ;
+}
+.a{font-size: large;
+font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+font-weight: 200;
+font-style:italic;
+color-scheme:orange ;
+text-align: center;
+color:indigo;
+background-image:linear-gradient(darkkhaki,lemonchiffon,papayawhip,peachpuff,darkkhaki);}
